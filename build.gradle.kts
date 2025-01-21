fe20d2d32eecd44544367a2bf7e1d8e406ff6111
David.Sasaki <david.sasaki@seattlecolleges.edu>
2025-01-21 05:15:14 +0000

    Add README Add gradle scripts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,9 @@
 plugins {
     id("java")
+    id("application")
 }
 
-group = "org.example"
+group = "org.zoo"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -10,8 +11,11 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation(platform("org.junit:junit-bom:5.11.4"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+}
+application{
+    mainClass = "org.zoo.ZooDemo"
 }
 
 tasks.test {
